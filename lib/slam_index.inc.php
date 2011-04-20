1dd6a398c2c46e2a59f7ebf63d54ce7df3c4615e
elihuihms <elihuihms@7dc1d8c8-8bba-7ab6-6bc6-7859c7d0d7ab>
2011-04-20 02:46:14 +0000

    $1

--- lib/slam_index.inc.php
+++ lib/slam_index.inc.php
@@ -1,27 +1,29 @@
 <?php
 
+set_include_path(get_include_path().PATH_SEPARATOR.'lib');
+
 /* class includes */
-require('lib/obj/slam_config.inc.php');
-require('lib/obj/slam_db.inc.php');
-require('lib/obj/slam_user.inc.php');
-require('lib/obj/slam_request.inc.php');
-require('lib/obj/slam_result.inc.php');
+require('obj/slam_config.inc.php');
+require('obj/slam_db.inc.php');
+require('obj/slam_user.inc.php');
+require('obj/slam_request.inc.php');
+require('obj/slam_result.inc.php');
 
 /* function includes */
-require('lib/logic/slam_functions.inc.php');
-require('lib/logic/slam_functions_user.inc.php');
-require('lib/logic/slam_functions_asset.inc.php');
-require('lib/logic/slam_functions_search.inc.php');
-require('lib/logic/slam_functions_dash.inc.php');
-require('lib/logic/slam_functions_files.inc.php');
+require('logic/slam_functions.inc.php');
+require('logic/slam_functions_user.inc.php');
+require('logic/slam_functions_asset.inc.php');
+require('logic/slam_functions_search.inc.php');
+require('logic/slam_functions_dash.inc.php');
+require('logic/slam_functions_files.inc.php');
 
 /* html renderers includes */
-require('lib/html/slam_html.inc.php');
-require('lib/html/slam_html_index.inc.php');
-require('lib/html/slam_html_user.inc.php');
-require('lib/html/slam_html_list.inc.php');
-require('lib/html/slam_html_asset.inc.php');
-require('lib/html/slam_html_search.inc.php');
-require('lib/html/slam_html_dash.inc.php');
+require('html/slam_html.inc.php');
+require('html/slam_html_index.inc.php');
+require('html/slam_html_user.inc.php');
+require('html/slam_html_list.inc.php');
+require('html/slam_html_asset.inc.php');
+require('html/slam_html_search.inc.php');
+require('html/slam_html_dash.inc.php');
 
 ?>
--- pub/about.php
+++ pub/about.php
@@ -1,4 +1,5 @@
 <?php
+	set_include_path(get_include_path().PATH_SEPARATOR.'../');
 	require('../lib/obj/slam_config.inc.php');
 	$config = new SLAMconfig(false);
 ?>
