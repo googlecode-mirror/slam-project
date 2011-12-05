be0863102262ad5c7b97cc8053658c313e2e55bb
elihuihms <elihuihms@7dc1d8c8-8bba-7ab6-6bc6-7859c7d0d7ab>
2011-12-05 02:58:36 +0000

    More installer work, table schemas

--- trunk/pub/password_choose.php
+++ trunk/pub/password_choose.php
@@ -5,9 +5,19 @@
 		<input type='hidden' name='user_name' value='<?php echo($_REQUEST['user_name']) ?>' />
 		<input type='hidden' name='secret' value='<?php echo($_REQUEST['secret']) ?>' />
 		<table>
-			<tr><td style='text-align:right'>New password:</td><td><input type='password' name='new_password' value='' id='new_password' /></td>&nbsp;</tr>
-			<tr><td style='text-align:right'>New password: (confirm)</td><td><input type='password' name='new_password_2' value='' id='new_password_2' /></td>&nbsp;</tr>
+			<tr>
+				 <td style='text-align:right'>New password:</td>
+				 <td>
+					 <input type='password' name='new_password' value='' id='new_password' />
+				 </td>
+			</tr>
+			<tr>
+				 <td style='text-align:right'>New password: (confirm)</td>
+				 <td>
+					  <input type='password' name='new_password_2' value='' id='new_password_2' />
+				 </td>
+			</tr>
 		</table>
 		<input type='button' value='Change Password' onClick="checkPasswordForm('changepassForm','new_password','new_password_2')" />
-	</form>
+	</div>
 </form>
